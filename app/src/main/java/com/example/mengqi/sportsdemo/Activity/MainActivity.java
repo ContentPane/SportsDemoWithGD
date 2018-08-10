2270bcea0f0a43e2bdf89a2acb910c5e9b16d41b
dingziheng <1519226623@qq.com>
2018-08-11 03:24:51 +0800

    first

--- app/src/main/java/com/example/mengqi/sportsdemo/Activity/MainActivity.java
+++ app/src/main/java/com/example/mengqi/sportsdemo/Activity/MainActivity.java
@@ -117,7 +117,6 @@ public class MainActivity extends AppCompatActivity implements LocationSource {
             }
         });
 
-
         mDlBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
