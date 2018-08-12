3413d5fbc99c19ed1c703aaedaf2a89f11eb5c2b
dingziheng <1519226623@qq.com>
2018-08-12 23:43:32 +0800

    first

--- app/src/main/java/com/example/mengqi/sportsdemo/Fragments/Fragment_freeMode.java
+++ app/src/main/java/com/example/mengqi/sportsdemo/Fragments/Fragment_freeMode.java
@@ -7,11 +7,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.mengqi.sportsdemo.R;
+import com.example.mengqi.sportsdemo.Utils.DrawUtils.DiffuseView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class Fragment_freeMode extends BaseFragment {
+    @BindView(R.id.spreadView_freerun)
+    DiffuseView mDiffuseView;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.freerun_fragment, null);
+        ButterKnife.bind(this,view);
+        mDiffuseView.start(); // 开始扩散
         return view;
     }
 }
--- app/src/main/java/com/example/mengqi/sportsdemo/Fragments/Fragment_randomMode.java
+++ app/src/main/java/com/example/mengqi/sportsdemo/Fragments/Fragment_randomMode.java
@@ -7,11 +7,19 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.mengqi.sportsdemo.R;
+import com.example.mengqi.sportsdemo.Utils.DrawUtils.DiffuseView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class Fragment_randomMode extends BaseFragment {
+    @BindView(R.id.spreadView_random)
+    DiffuseView mDiffuseView;
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.freerun_fragment, null);
+        View view = inflater.inflate(R.layout.randomrun_fragment, null);
+        ButterKnife.bind(this,view);
+        mDiffuseView.start();
         return view;
     }
 }
